2cef943967073588e92fee94f0e6b1c356accd37
Matt Wells <mattwells@users.noreply.github.com>
2018-04-26 18:00:25 +0100

    Lot of WHM Gear for Romnom

--- Romnomnom_WHM.lua
+++ Romnomnom_WHM.lua
@@ -1,88 +1,119 @@
 function get_sets()
-
 	sets.Idle = {
-		main="Tamaxchi", sub="Genbu's Shield", ammo="Incantor Stone",
-		head="Gendewitha caubeen +1", neck="Orison Locket", ear1="Roundel Earring", ear2="Loquac. Earring",
-		body="Gendewitha bliaut +1", hands="Iuvenalis mittens +1", ring1="Serket Ring", ring2="Prolix Ring",
-		back="Orison Cape", waist="Cascade Belt", legs="Piety Pantaloons", feet="Espial socks",
+		main = "Queller Rod",
+		sub = "Sors Shield",
+		ammo = "Incantor Stone",
+		head = "Inyanga Tiara +1",
+		body = {
+			name = "Gende. Bilaut +1",
+			augments = {"Phys. dmg. taken -1%", '"Cure" potency +3%'}
+		},
+		hands = "Inyan. Dastanas +1",
+		legs = "Assiduity Pants",
+		feet = "Inyan. Crackows +1",
+		neck = "Phalaina Locket",
+		waist = "Cascade Belt",
+		left_ear = "Roundel Earring",
+		right_ear = "Loquac. Earring",
+		left_ring = "Serket Ring",
+		right_ring = "Prolix Ring",
+		back = {
+			name = "Alaunus's Cape",
+			augments = {"MND+20", "Eva.+20 /Mag. Eva.+20", "MND+10", '"Cure" potency +10%', "Damage taken-3%"}
+		}
 	}
 
 	sets.Precast = {}
 
 	sets.Precast.FastCast = {
-		ammo="Incantor Stone",
-		neck="Orison Locket", ear2="Loquac. Earring",
-		body="Shango Robe",
-		ring2="Prolix Ring",
+		ammo = "Incantor Stone",
+		body = "Inyanga Jubbah +1",
+		ear2 = "Loquac. Earring",
+		ring2 = "Prolix Ring",
+		neck = "Orison Locket",
+		waist = "Witful Belt",
+		right_ear = "Loquac. Earring",
+		right_ring = "Prolix Ring"
 	}
 
-	sets.Precast.Cure = {
-		body="Heka's Kalasiris", neck="Orison Locket", ear2="Loquac. Earring",
-		ring2="Prolix Ring",
-		feet="Cure Clogs",
-	}
+	sets.Precast.Cure = set_combine(
+		sets.Precast.FastCast,
+		{
+			main = "Queller Rod",
+			sub = "Sors Shield",
+			body = "Heka's Kalasiris",
+			ear1 = "Nourish. Earring +1",
+			legs = "Ebers Pantaloons",
+			feet = "Cure Clogs"
+		}
+	)
 
 	sets.Midcast = {}
 
 	sets.Midcast.Cure = {
-		main="Tamaxchi", sub="Genbu's Shield",
-		head="Gendewitha caubeen +1", neck="Phalaina locket", ear1="Roundel Earring",
-		body="Heka's Kalasiris", hands="Augur's gloves",
-		back="Orison Cape", waist="Cascade Belt", legs="Orsn. Pantaln. +2", feet="Orison Duckbills +2",
+		main = {
+			name = "Queller Rod",
+			augments = {"Healing magic skill +15", '"Cure" potency +10%', '"Cure" spellcasting time -7%'}
+		},
+		sub = "Sors Shield",
+		head = {name = "Gende. Caubeen +1", augments = {"Phys. dmg. taken -4%", "Song recast delay -1"}},
+		body = "Heka's Kalasiris",
+		hands = "Augur's Gloves",
+		legs = "Ebers Pantaloons",
+		feet = "Ebers Duckbills",
+		neck = "Colossus's Torque",
+		waist = "Cascade Belt",
+		left_ear = "Nourish. Earring +1",
+		right_ear = "Psystorm Earring",
+		back = {
+			name = "Alaunus's Cape",
+			augments = {"MND+20", "Eva.+20 /Mag. Eva.+20", "MND+10", '"Cure" potency +10%', "Damage taken-3%"}
+		}
 	}
 
 	sets.Midcast.Bar = {
-		legs="Cleric's pantaloons +2",
+		legs = "Piety pantaloons"
 	}
 end
 
 function precast(spell, action)
-	if spell.type:endswith('Magic') or spell.type == "BardSong" or spell.type == "Ninjutsu" then
-
+	if spell.type:endswith("Magic") or spell.type == "BardSong" or spell.type == "Ninjutsu" then
 		if buffactive.silence then -- Cancel Magic or Ninjutsu or BardSong If You Are Silenced or Out of Range --
 			cancel_spell()
-			debug(spell.name..' Canceled: [Silence has fallen]')
+			debug(spell.name .. " Canceled: [Silence has fallen]")
 			return
 		else
-
-			if string.find(spell.english,'Cur') and spell.english ~= "Cursna" then
+			if string.find(spell.english, "Cur") and spell.english ~= "Cursna" then
 				equip(sets.Precast.Cure)
-				debug('Precast: Cure')
-
-			elseif string.find(spell.english, 'Bar') then
+				debug("Precast: Cure")
+			elseif string.find(spell.english, "Bar") then
 				equip(sets.Midcast.Bar)
-				debug('Midcast: Bar')
-
+				debug("Midcast: Bar")
 			else
 				equip(sets.Precast.FastCast)
-				debug('Precast: Fast Cast')
+				debug("Precast: Fast Cast")
 			end
-
 		end
 	end
 end
 
 function midcast(spell, action)
-	if spell.type:endswith('Magic') or spell.type == 'Ninjutsu' or spell.type == "BardSong" then
-
-		if string.find(spell.english,'Cure') then
+	if spell.type:endswith("Magic") or spell.type == "Ninjutsu" or spell.type == "BardSong" then
+		if string.find(spell.english, "Cure") then
 			equip(sets.Midcast.Cure)
-			debug('Midcast: Cure')
-
-		elseif string.find(spell.english,'Cura') then
+			debug("Midcast: Cure")
+		elseif string.find(spell.english, "Cura") then
 			equip(sets.Midcast.Curaga)
-			debug('Midcast: Curaga')
-
-		elseif string.find(spell.english, 'Bar') then
+			debug("Midcast: Curaga")
+		elseif string.find(spell.english, "Bar") then
 			equip(sets.Midcast.Bar)
-			debug('Midcast: Bar')
+			debug("Midcast: Bar")
 		end
-
 	end
 end
 
 function aftercast(spell, action)
-	debug('Idle')
+	debug("Idle")
 	equip(sets.Idle)
 end
 
