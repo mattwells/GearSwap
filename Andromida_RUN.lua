b2d0d13aab86435e51e84e9e291ac9ce0c3f4c3b
Matt Wells <mattwells@users.noreply.github.com>
2019-03-24 20:27:36 +0000

    Idle, TP and Resolution sets

--- Andromida_RUN.lua
+++ Andromida_RUN.lua
@@ -1,7 +1,63 @@
 function get_sets()
-    sets.Idle = {}
+    sets.Idle = {
+        ammo="Staunch Tathlum +1",
+        head="Meghanada Visor +2",
+        body="Meg. Cuirie +2",
+        hands="Meg. Gloves +2",
+        legs="Meg. Chausses +2",
+        feet="Meg. Jam. +2",
+        neck="Loricate Torque +1",
+        waist="Flume Belt",
+        left_ear="Telos Earring",
+        right_ear="Sherida Earring",
+        left_ring="Moonlight Ring",
+        right_ring="Defending Ring",
+        back={ 
+            name="Ogma's cape", 
+            augments={
+                'STR+20',
+                'Accuracy+20 Attack+20',
+                'STR+10',
+                '"Dbl.Atk."+10',
+            }
+        },
+    }
 
-    sets.Engaged = {}
+    sets.Engaged = {
+        ammo="Ginsen",
+        head={ 
+            name="Adhemar Bonnet +1",
+            augments={'DEX+12','AGI+12','Accuracy+20',}
+        },
+        body={ 
+            name="Adhemar Jacket +1", 
+            augments={'STR+12','DEX+12','Attack+20',}
+        },
+        hands={ 
+            name="Adhemar Wrist. +1",
+            augments={'STR+12','DEX+12','Attack+20',}
+        },
+        legs="Samnuha Tights",
+        feet={ 
+            name="Carmine Greaves +1", 
+            augments={'Accuracy+12','DEX+12','MND+20',}
+        },
+        neck="Anu Torque",
+        waist="Windbuffet Belt +1",
+        left_ear="Telos Earring",
+        right_ear="Sherida Earring",
+        left_ring="Niqmaddu Ring",
+        right_ring="Epona's Ring",
+        back={ 
+            name="Ogma's cape",
+            augments={
+                'STR+20',
+                'Accuracy+20 Attack+20',
+                'STR+10',
+                '"Dbl.Atk."+10',
+            }
+        },
+    }
 
     sets.Rune = {}
     sets.Ward = {}
@@ -28,13 +84,69 @@ function get_sets()
     -- sets.WeaponSkill['Ground Strike'] = {}
     -- sets.WeaponSkill['Herculean Slash'] = {}
 
+    sets.WeaponSkill['Resolution'] = {
+        main="Aettir",
+        sub="Utu Grip",
+        ammo="Knobkierrie",
+        head={ 
+            name="Lilitu Headpiece", 
+            augments={
+                'STR+10',
+                'DEX+10','Attack+15',
+                'Weapon skill damage +3%',
+            }
+        },
+        body={ 
+            name="Adhemar Jacket +1", 
+            augments={'STR+12','DEX+12','Attack+20',}
+        },
+        hands={ 
+            name="Herculean Gloves", 
+            augments={'"Store TP"+5','STR+7','Accuracy+14',}
+        },
+        legs="Meg. Chausses +2",
+        feet={ 
+            name="Lustra. Leggings +1", 
+            augments={'Attack+20','STR+8','"Dbl.Atk."+3',}
+        },
+        neck="Fotia Gorget",
+        waist="Flume Belt",
+        left_ear="Moonshade Earring",
+        right_ear="Sherida Earring",
+        left_ring="Niqmaddu Ring",
+        right_ring="Regal Ring",
+        back={ 
+            name="Ogma's cape", 
+            augments={
+                'STR+20',
+                'Accuracy+20 Attack+20',
+                'STR+10',
+                '"Dbl.Atk."+10',
+            }
+        },
+    }
+
+    sets.FastCast = {
+        head="Runeist Bandeau",
+        hands={ 
+            name="Leyline Gloves", 
+            augments={'"Fast Cast"+2',}
+        },
+        feet="Carmine Greaves +1",
+        right_ear="Loquac. Earring",
+        left_ring="Prolix Ring",
+    }
+
+    sets.Midcast = {}
+    sets.Midcast["Enhancing Magic"] = {
+        hands = "Runeist Mitons +1",
+        legs = "Carmine Cuisses +1",
+        waist = "Olympus Sash",
+        left_ear = "Andoaa Earring",
+    }
 end
 
 function precast(spell, action)
-    if "Trust" == spell.type then
-        return
-    end
-
     if (spell.english == "Spectral Jig" or spell.english == "Sneak") and buffactive.Sneak then
         cast_delay(0.2)
         send_command("cancel Sneak")
@@ -52,11 +164,32 @@ function precast(spell, action)
         return
     end
 
+    if is_magic(spell) then
+        equip(sets.FastCast)
+        return
+    end
+
     debug("No set for " .. spell.type .. "." .. spell.english)
 end
 
-function aftercast(spell, action)
-    status_change(player.status)
+function midcast(spell, action)
+    if (~is_magic(spell)) then
+        return
+    end
+    
+    if (sets["Midcast"][spell.skill] and sets["Midcast"][spell.skill][spell.english]) then
+        equip(sets["Midcast"][spell.skill][spell.english])
+
+        return 
+    end
+
+    if sets["Midcast"][spell.skill] and sets["Midcast"][spell.skill].equipable then
+        equip(sets["Midcast"][spell.skill])
+
+        return
+    end
+
+    debug("No set for Midcast." .. spell.skill .. "." .. spell.english)
 end
 
 function status_change(new, old)
@@ -69,12 +202,8 @@ function status_change(new, old)
     end
 end
 
-function status_change_engaged()
-    equip(sets.Engaged)
-
-    if DT then
-        equip(sets.PDT)
-    end
+function aftercast(spell, action)
+    status_change(player.status)
 end
 
 function buff_change(name, gain, buff_details)
