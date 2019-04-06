4cfc6d8c7eeb70427eaa66cedad73c5063e433ec
Matt Wells <mattwells@users.noreply.github.com>
2019-04-06 22:38:39 +0100

    Lots of changes to RUN

--- Andromida_RUN.lua
+++ Andromida_RUN.lua
@@ -1,63 +1,69 @@
 function get_sets()
+    send_command("@input /macro book 6;wait .1;input /macro set 1")
+
     sets.Idle = {
-        ammo="Staunch Tathlum +1",
-        head="Meghanada Visor +2",
-        body="Meg. Cuirie +2",
-        hands="Meg. Gloves +2",
-        legs="Meg. Chausses +2",
-        feet="Meg. Jam. +2",
-        neck="Loricate Torque +1",
-        waist="Flume Belt",
-        left_ear="Telos Earring",
-        right_ear="Sherida Earring",
-        left_ring="Moonlight Ring",
-        right_ring="Defending Ring",
-        back={ 
-            name="Ogma's cape", 
-            augments={
-                'STR+20',
-                'Accuracy+20 Attack+20',
-                'STR+10',
-                '"Dbl.Atk."+10',
+        ammo = "Staunch Tathlum +1",
+        head = "Meghanada Visor +2",
+        body = "Meg. Cuirie +2",
+        hands = "Meg. Gloves +2",
+        legs = "Meg. Chausses +2",
+        feet = "Meg. Jam. +2",
+        neck = "Futhark Torque +2",
+        waist = "Flume Belt",
+        left_ear = "Telos Earring",
+        right_ear = "Sherida Earring",
+        left_ring = "Moonlight Ring",
+        right_ring = "Defending Ring",
+        back = {
+            name = "Ogma's cape",
+            augments = {
+                "STR+20",
+                "Accuracy+20 Attack+20",
+                "STR+10",
+                '"Dbl.Atk."+10'
             }
-        },
+        }
     }
 
-    sets.Engaged = {
-        ammo="Ginsen",
-        head={ 
-            name="Adhemar Bonnet +1",
-            augments={'DEX+12','AGI+12','Accuracy+20',}
+    sets.Engaged = {mode = "Melee"}
+    sets.Engaged.Melee = {
+        ammo = "Ginsen",
+        head = {
+            name = "Adhemar Bonnet +1",
+            augments = {"DEX+12", "AGI+12", "Accuracy+20"}
         },
-        body={ 
-            name="Adhemar Jacket +1", 
-            augments={'STR+12','DEX+12','Attack+20',}
+        body = {
+            name = "Adhemar Jacket +1",
+            augments = {"STR+12", "DEX+12", "Attack+20"}
         },
-        hands={ 
-            name="Adhemar Wrist. +1",
-            augments={'STR+12','DEX+12','Attack+20',}
+        hands = {
+            name = "Adhemar Wrist. +1",
+            augments = {"STR+12", "DEX+12", "Attack+20"}
         },
-        legs="Samnuha Tights",
-        feet={ 
-            name="Carmine Greaves +1", 
-            augments={'Accuracy+12','DEX+12','MND+20',}
+        legs = "Samnuha Tights",
+        feet = {
+            name = "Carmine Greaves +1",
+            augments = {"Accuracy+12", "DEX+12", "MND+20"}
         },
-        neck="Anu Torque",
-        waist="Windbuffet Belt +1",
-        left_ear="Telos Earring",
-        right_ear="Sherida Earring",
-        left_ring="Niqmaddu Ring",
-        right_ring="Epona's Ring",
-        back={ 
-            name="Ogma's cape",
-            augments={
-                'STR+20',
-                'Accuracy+20 Attack+20',
-                'STR+10',
-                '"Dbl.Atk."+10',
+        neck = "Anu Torque",
+        waist = "Windbuffet Belt +1",
+        left_ear = "Telos Earring",
+        right_ear = "Sherida Earring",
+        left_ring = "Niqmaddu Ring",
+        right_ring = "Epona's Ring",
+        back = {
+            name = "Ogma's cape",
+            augments = {
+                "STR+20",
+                "Accuracy+20 Attack+20",
+                "STR+10",
+                '"Dbl.Atk."+10'
             }
-        },
+        }
     }
+    sets.Engaged.Parry = {}
+    sets.Engaged.Magic = {}
+    sets.Engaged.Hybrid = {}
 
     sets.Rune = {}
     sets.Ward = {}
@@ -84,57 +90,64 @@ function get_sets()
     -- sets.WeaponSkill['Ground Strike'] = {}
     -- sets.WeaponSkill['Herculean Slash'] = {}
 
-    sets.WeaponSkill['Resolution'] = {
-        main="Aettir",
-        sub="Utu Grip",
-        ammo="Knobkierrie",
-        head={ 
-            name="Lilitu Headpiece", 
-            augments={
-                'STR+10',
-                'DEX+10','Attack+15',
-                'Weapon skill damage +3%',
+    sets.WeaponSkill["Resolution"] = {
+        main = "Aettir",
+        sub = "Utu Grip",
+        ammo = "Knobkierrie",
+        head = {
+            name = "Lilitu Headpiece",
+            augments = {
+                "STR+10",
+                "DEX+10",
+                "Attack+15",
+                "Weapon skill damage +3%"
             }
         },
-        body={ 
-            name="Adhemar Jacket +1", 
-            augments={'STR+12','DEX+12','Attack+20',}
+        body = {
+            name = "Adhemar Jacket +1",
+            augments = {"STR+12", "DEX+12", "Attack+20"}
         },
-        hands={ 
-            name="Herculean Gloves", 
-            augments={'"Store TP"+5','STR+7','Accuracy+14',}
+        hands = {
+            name = "Adhemar Wrist. +1",
+            augments = {"STR+12", "DEX+12", "Attack+20"}
         },
-        legs="Meg. Chausses +2",
-        feet={ 
-            name="Lustra. Leggings +1", 
-            augments={'Attack+20','STR+8','"Dbl.Atk."+3',}
+        legs = "Meg. Chausses +2",
+        feet = {
+            name = "Lustra. Leggings +1",
+            augments = {"Attack+20", "STR+8", '"Dbl.Atk."+3'}
         },
-        neck="Fotia Gorget",
-        waist="Flume Belt",
-        left_ear="Moonshade Earring",
-        right_ear="Sherida Earring",
-        left_ring="Niqmaddu Ring",
-        right_ring="Regal Ring",
-        back={ 
-            name="Ogma's cape", 
-            augments={
-                'STR+20',
-                'Accuracy+20 Attack+20',
-                'STR+10',
-                '"Dbl.Atk."+10',
+        neck = "Fotia Gorget",
+        waist = "Fotia Belt",
+        left_ear = "Moonshade Earring",
+        right_ear = "Sherida Earring",
+        left_ring = "Niqmaddu Ring",
+        right_ring = "Regal Ring",
+        back = {
+            name = "Ogma's cape",
+            augments = {
+                "STR+20",
+                "Accuracy+20 Attack+20",
+                "STR+10",
+                '"Dbl.Atk."+10'
             }
-        },
+        }
     }
 
     sets.FastCast = {
-        head="Runeist Bandeau",
-        hands={ 
-            name="Leyline Gloves", 
-            augments={'"Fast Cast"+2',}
+        ammo = "Impatiens",
+        head = "Runeist Bandeau",
+        hands = {
+            name = "Leyline Gloves",
+            augments = {'"Fast Cast"+2'}
         },
-        feet="Carmine Greaves +1",
-        right_ear="Loquac. Earring",
-        left_ring="Prolix Ring",
+        legs = "Aya. Cosciales +2",
+        feet = "Carmine Greaves +1",
+        neck = "Futhark Torque +2",
+        waist = "Flume Belt",
+        left_ear = "Magnetic Earring",
+        right_ear = "Loquac. Earring",
+        left_ring = "Prolix Ring",
+        right_ring = "Defending Ring"
     }
 
     sets.Midcast = {}
@@ -142,7 +155,7 @@ function get_sets()
         hands = "Runeist Mitons +1",
         legs = "Carmine Cuisses +1",
         waist = "Olympus Sash",
-        left_ear = "Andoaa Earring",
+        left_ear = "Andoaa Earring"
     }
 end
 
@@ -176,11 +189,11 @@ function midcast(spell, action)
     if not is_magic(spell) then
         return
     end
-    
+
     if sets["Midcast"][spell.skill] and sets["Midcast"][spell.skill][spell.english] then
         equip(sets["Midcast"][spell.skill][spell.english])
 
-        return 
+        return
     end
 
     if sets["Midcast"][spell.skill] and sets["Midcast"][spell.skill].equipable then
@@ -202,6 +215,10 @@ function status_change(new, old)
     end
 end
 
+function status_change_engaged()
+    equip(sets.Engaged[sets.Engaged.mode])
+end
+
 function aftercast(spell, action)
     status_change(player.status)
 end
@@ -210,27 +227,47 @@ function buff_change(name, gain, buff_details)
     debug(name .. " " .. (gain and "on" or "off"))
 end
 
-function debug(s)
-    send_command("@input /echo " .. s)
-end
+function self_command(argsString)
+    print(T(player).status)
 
-function self_command(command)
-    command = command:lower()
+    args = argsString:lower():split(" ")
 
-    if "dt" == command then
-        DT = not DT
-
-        windower.add_to_chat(123, "DT: " .. (DT and "on" or "off"))
-
-        status_change(player.status, player.status)
-
-        return
+    if _G["self_command_" .. args[1]] then
+        _G["self_command_" .. args[1]](args:slice(2))
     end
 end
 
+function self_command_engaged(args)
+    if not args[1] then
+        error(4, "Error: No Engaged Mode Specified")
+        return
+    end
+
+    local mode = args[1]:ucfirst()
+    if not sets.Engaged[mode] then
+        error(4, "Error: Invalid Engaged Mode: " .. mode)
+        return
+    end
+
+    sets.Engaged.mode = mode
+    status_change(player.status)
+    notce("Engaged Mode Set: " .. mode)
+end
+
 function is_magic(spell)
-    return spell.type:endswith("Magic") 
-        or spell.type == "BardSong" 
-        or spell.type == "Ninjutsu" 
-        or spell.type == "Trust"
+    return spell.type:endswith("Magic") or spell.type == "BardSong" or spell.type == "Ninjutsu" or spell.type == "Trust"
+end
+
+function notice(s)
+    add_to_chat(121, s)
+end
+
+function error(s)
+    add_to_chat(4, s)
+end
+
+function debug(s)
+    if debugMode then
+        notice(s)
+    end
 end
